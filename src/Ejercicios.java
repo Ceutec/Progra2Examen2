af4aa8b709c99edca8cd90949076bfb581766b14
Turupawn <turupawn@userr>
2014-09-23 07:07:04 -0600

    primer

---
+++ src/Ejercicios.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Ejercicios {
+	
+	//Devuelve true si todos los elementos de 'mi_lista' son pares, de lo contrario devuelve false
+	static boolean sonPares(ArrayList<Integer> mi_lista)
+	{
+		return false;
+	}
+	
+	//Devuelve el numero menor almacenado en 'mi_lista'
+	static int getMenor(ArrayList<Integer> mi_lista)
+	{
+		return -1;
+	}
+	
+	//Devuelve el numero menor de un archivo
+	//Dado, el archivo unicamente contiene enteros
+	static int getNumeroMayor(String nombre_archivo)
+	{
+		return -1;
+	}
+	
+	//Devuelve cuantas veces existe la string buscada en el archivo con nombre nombre_archivo
+	static int cuantasVecesExiste(String nombre_archivo, String buscada)
+	{
+		return -1;
+	}
+}
